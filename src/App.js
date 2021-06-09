cfb439d920a84d1292c3bff5b06e01f0c0c1acb3
luis banda <rockband_666@hotmail.com>
2021-06-09 01:55:34 -0400

    realizar cartas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import React from 'react';
+import Card from './components/Card';
+import Button from './components/Button';
+import getContentCards from './assets/getContentCards';
+import './global.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const data= getContentCards();
+    // console.log(data);
+    // const card = data[0];
+    const [quantity, setQuantity] = useState(5);
+    const remove =() =>{
+        setQuantity(quantity - 1);
+    }
+    const add =() =>{
+        setQuantity(quantity + 1);
+    }
+    const card=[];
+    for(let i = 0 ; i < quantity ; i++){
+        card.push(<Card data = {card[i]} />);
+    }
+    return (
+        <>
+            <section className = "button-container">
+                <button 
+                content ="Remove Card" 
+                id ="removeCard" 
+                click={remove}
+                disabled = {quantity <= 0}
+                />
+                {quantity}
+                <button 
+                content ="Add Card" 
+                id ="addCard" 
+                click={add}
+                disabled = {quantity >= 5}
+                />
+            </section>
+            <section className='container-cards'>
+            {cards }
+            {
+                // data.map((card)=>{
+                // return <Card data = {card} />;
+                // })
+            }
+            </section>
+        </>
+    );
 }
 
 export default App;
